f4df8ac62b8d5e8371684252e7d841c5bd1d9d29
David Ingram <david@ingramaviation.com>
2020-01-22 11:49:33 -0600

    added nav bar and welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'application#welcome'
   devise_for :users, :controllers => {:registrations => "registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end 
-  
+
 end
