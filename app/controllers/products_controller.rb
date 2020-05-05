01780a707690046eea71c52b54429741eaf2f802
David Ingram <david@ingramaviation.com>
2020-05-05 15:26:36 -0500

    Added drop menu for product_name to purchases.new view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+
+    before_action :set_product, only: [:show, :edit, :update]
+    
+    def index
+        @products = Product.all
+        @user = current_user
+    end
+
+    def show
+        @user = current_user
+    end
+
+    def new
+        if !current_user.admin
+            redirect_to products_path
+        else
+            @product = Product.new
+        end
+    end
+
+    def create
+        @product = Product.create(product_params)
+        redirect_to product_path(@product)
+    end
+
+    def edit
+        redirect_to product_path(@aproduct) if !current_user.admin
+    end
+
+    def update
+        @product.update(product_params)
+        redirect_to product_path(@aproduct)
+    end
+
+    private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
+    end
+
+    def set_attraction
+        @attraction = Attraction.find(params[:id])
+    end
+
 end
